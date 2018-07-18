02c12a19779ce7e2671c223661fdda2299dd69b5
julienwhite <julienwhiteinfo@gmail.com>
2018-07-18 16:32:59 +1000

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/todos', (req,res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
-
+module.exports = {app};
 /* DOC
     http://mongoosejs.com/docs/validation.html
     http://mongoosejs.com/docs/guide.html
