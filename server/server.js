e30bfc63eda4b9d8e2c8ee9a93a5eee4a7373429
julienwhite <julienwhiteinfo@gmail.com>
2018-07-23 15:08:09 +1000

    Added PATCH server logic

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+/* DOC
+    http://mongoosejs.com/docs/validation.html
+    http://mongoosejs.com/docs/guide.html
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -74,11 +80,33 @@ app.delete('/todos/:id', (req,res) => {
   })
 });
 
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  console.log(`--> PATCH /todos/${id}`);
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set : body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(404).send();
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 module.exports = {app};
-/* DOC
-    http://mongoosejs.com/docs/validation.html
-    http://mongoosejs.com/docs/guide.html
-*/
